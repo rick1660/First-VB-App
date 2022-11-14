ccdb16f7a445afee2de06153a3bd30805270e0ca
Ricardo Jacuinde <71536847+rick1660@users.noreply.github.com>
2022-11-14 14:09:12 -0800

    dd
    first

---
+++ Interfaz inventario/WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.dgv1 = New System.Windows.Forms.DataGridView()
+        CType(Me.dgv1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl1.Location = New System.Drawing.Point(238, 78)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(577, 37)
+        Me.lbl1.TabIndex = 0
+        Me.lbl1.Text = "Mi primera conexion a una BD en VB"
+        '
+        'dgv1
+        '
+        Me.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv1.Location = New System.Drawing.Point(97, 181)
+        Me.dgv1.Name = "dgv1"
+        Me.dgv1.Size = New System.Drawing.Size(823, 317)
+        Me.dgv1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1068, 688)
+        Me.Controls.Add(Me.dgv1)
+        Me.Controls.Add(Me.lbl1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgv1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbl1 As Label
+    Friend WithEvents dgv1 As DataGridView
+End Class
---
+++ Interfaz inventario/WindowsApp1/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    'Codigo necesario para la conexion con MSSQL'
+    Inherits System.Windows.Forms.Form
+    'Create ADO.NET objects.
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private myReader As SqlDataReader
+    Private results As String
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Crear un objeto de conexion'
+        myConn = New SqlConnection("Initial Catalog=Ejemplo;" & "Data Source=localhost;Integrated Security=SSPI;")
+
+        'Create a Command object.
+        myCmd = myConn.CreateCommand
+        myCmd.CommandText = "SELECT * from empleados"
+
+        'Open the connection.
+        myConn.Open()
+
+
+        'Recuperacion de los datos'
+        myReader = myCmd.ExecuteReader()
+
+
+        'Concatenate the query result into a string.
+        Do While myReader.Read()
+            results = results & myReader.GetString(0) & vbTab &
+            myReader.GetString(1) & vbLf
+        Loop
+        'Display results.
+        MsgBox(results)
+
+
+        'Close the reader and the database connection.
+        myReader.Close()
+        myConn.Close()
+
+
+
+
+    End Sub
+
+    Private Sub lbl1_Click(sender As Object, e As EventArgs) Handles lbl1.Click
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv1.CellContentClick
+
+    End Sub
+End Class
